ddd553678393199dd2b1648daabdca1785a6ed4c
BarendUngrodt <ungrodtb@msu.edu>
2014-10-24 18:21:59 -0400

    created toolbarPanel class

---
+++ Nematode/src/com/nematode/gui/ToolbarPanel.java
@@ -0,0 +1,28 @@
+package com.nematode.gui;
+
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+
+public class ToolbarPanel extends ExtendableJPanel {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ToolbarPanel() {
+		this.setName("toolbarPanel");
+
+		addBorder();
+	}
+
+	private void addBorder() {
+		final Border raisedBevelBorder = BorderFactory.createRaisedBevelBorder();
+		final Border loweredBevelBorder = BorderFactory.createLoweredBevelBorder();
+		final CompoundBorder compoundBorder = BorderFactory.createCompoundBorder(raisedBevelBorder,
+				loweredBevelBorder);
+		this.setBorder(compoundBorder);
+	}
+
+}
---
+++ NematodeTest/testSrc/com/nematode/gui/ToolbarPanelTest.java
@@ -0,0 +1,23 @@
+package com.nematode.gui;
+
+import javax.swing.border.CompoundBorder;
+
+import org.junit.Test;
+
+import com.nematode.unittesting.AssertTestCase;
+
+public class ToolbarPanelTest extends AssertTestCase {
+
+	@Test
+	public void testExtendableJFrame() throws Exception {
+		assertExtends(ExtendableJPanel.class, ToolbarPanel.class);
+	}
+
+	@Test
+	public void testConstructorCorrectlySetsUpPanel() throws Exception {
+		final ToolbarPanel toolbarPanel = new ToolbarPanel();
+		assertEquals("toolbarPanel", toolbarPanel.getName());
+		assertEquals(0, toolbarPanel.getComponentCount());
+		assertIsOfTypeAndGet(CompoundBorder.class, toolbarPanel.getBorder());
+	}
+}
